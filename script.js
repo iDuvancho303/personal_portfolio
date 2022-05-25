e1f2631d742a5f6202d5746e2e0e9cf70f56d10b
duvancho303 <duvancho303@gmail.com>
2022-05-24 20:01:00 -0600

    adding more code

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const name = document.getElementById('name')
+const firstName = document.getElementById('name')
 const lastName = document.getElementById('last')
 const email = document.getElementById('email')
 const phoneNumber = document.getElementById('phone')
 const textArea = document.getElementById('comments')
 const myForm = document.getElementById('myform')
 
-const errorElement = document.getElementById('')
+const errorElement = document.getElementById('error')
 
 myForm.addEventListener('submit', (e) => {
     let messages = []
-    if (name.value === '' || name.value === null) {
+    if (firstName.value === '' || firstName.value === null) {
         messages.push('Name is required')
     }
 
     if (messages.length > 0) {
         e.preventDefault()
+        errorElement.innerText = messages.join(', ')
     }
-    e.preventDefault() 
 
 
 
